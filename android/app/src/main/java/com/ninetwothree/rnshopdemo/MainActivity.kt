83cf4a6eba1a3bd7a959096c30e5accd82855208
Mikhail Kuzmenko <mikhail@ninetwothree.co>
2025-06-20 17:39:54 +0300

    renamed project to Shop Demo

--- android/app/src/main/java/com/ninetwothree/rnshopdemo/MainActivity.kt
+++ android/app/src/main/java/com/ninetwothree/rnshopdemo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.reactnativeboilerplate
+package com.ninetwothree.rnshopdemo
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ReactNativeBoilerplate"
+  override fun getMainComponentName(): String = "rn-shop-demo"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/ninetwothree/rnshopdemo/MainApplication.kt
+++ android/app/src/main/java/com/ninetwothree/rnshopdemo/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.reactnativeboilerplate
+package com.ninetwothree.rnshopdemo
 
 import android.app.Application
 import com.facebook.react.PackageList
